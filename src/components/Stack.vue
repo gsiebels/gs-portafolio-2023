43b2d4f0a50cf4d780c6e0dfd85823c12e30cb45
Gerardo Siebels <gsiebels@gmail.com>
2023-06-21 22:10:37 +0100

    added a different logic for the zooming

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -1,42 +1,55 @@
 <script setup>
+  import { onMounted, ref } from 'vue'
+  import SkillLevel from './SkillLevel.vue'
+
+  const skillOn = ref(false)
+
+  onMounted(() => {
+    const urlParam = window.location.search
+
+    if (urlParam === '?skill') skillOn.value = true
+
+  })
+
+
   const langList = [
-  {
-    name: 'HTML',
-    img: 'logoHtml.svg',
-    level: 5
-  },
-  {
-    name: 'CSS',
-    img: 'logoCss.svg',
-    level: 5
-  },
-  {
-    name: 'JavaScript',
-    img: 'logoJs.svg',
-    level: 4
-  },
-  {
-    name: 'Vue.js',
-    img: 'logoVue.svg',
-    level: 4
-  },
-  {
-    name: 'Nuxt.js',
-    img: 'logoNuxt.svg',
-    level: 4
-  },
-  {
-    name: 'Python',
-    img: 'logoPython.svg',
-    level: 3
-  },
-  {
-    name: 'Django',
-    img: 'logoDjango.svg',
-    level: 2
-  },
-  
-]
+    {
+      name: 'HTML',
+      img: 'logoHtml.svg',
+      level: 5
+    },
+    {
+      name: 'CSS',
+      img: 'logoCss.svg',
+      level: 5
+    },
+    {
+      name: 'JavaScript',
+      img: 'logoJs.svg',
+      level: 4
+    },
+    {
+      name: 'Vue.js',
+      img: 'logoVue.svg',
+      level: 4
+    },
+    {
+      name: 'Nuxt.js',
+      img: 'logoNuxt.svg',
+      level: 4
+    },
+    {
+      name: 'Python',
+      img: 'logoPython.svg',
+      level: 3
+    },
+    {
+      name: 'Django',
+      img: 'logoDjango.svg',
+      level: 2
+    },
+  ]
+
 </script>
 
 <template>
@@ -46,7 +59,7 @@
           <img class="lang-logo" :src="`/src/assets/logos/${lang.img}`" :alt="`${lang.name} icon`">
         </div>
         <p class="lang-name">{{ lang.name }}</p>
-        <dir class="lang-level">{{ lang.level }}</dir>
+        <SkillLevel v-if="skillOn" :level="lang.level" />
       </div>
   </div>
 </template>
@@ -88,12 +101,4 @@
     color: white;
   }
 
-  .lang-level {
-    display: flex;
-    padding: 0;
-    background: #373737;
-    border: 2px solid #868686;
-    width: 100px;
-    border-radius: 15px;
-  }
 </style>
